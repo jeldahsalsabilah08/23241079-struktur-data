9cb650edd593b5cf4b514e9b5d1178597f8589de
jeldahsalsabilah08 <147676414+jeldahsalsabilah08@users.noreply.github.com>
2024-05-16 14:05:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+#Perbandingan
+#lebih dari >
+#kurang dari<
+#lebih dari sama dengan >=
+#kurang dari sama dengan <=
+#sama dengan ==
+#tidak sama dengan!=
+#is
+#is not
+
+a = 3
+b = 6 
+
+hasil = a > b
+print (a,'>',b, '= ', hasil)
+hasil = a < b
+print (a,'<',b, '= ',hasil)
+hasil = a >= b
+print (a,'>=',b, '= ',hasil)
+hasil = a <= b
+print (a,'<=',b, '= ',hasil)
+hasil = a == b
+print (a, '==',b, '= ' ,hasil)
+hasil = a != b
+print(a, '!=',b, '= ',hasil)
+hasil = a is b
+print (a, 'is' ,b, '= ',hasil)
+hasil = a is not b 
+print (a, 'is not',b, '= ',hasil')
+
+
+
+
+
